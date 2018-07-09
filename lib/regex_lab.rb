bb83567e25592f483a1adc8c76bc88fd1c5c7eb9
Azhar Shah <azharshahstl@yahoo.com>
2018-07-09 15:38:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,19 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
-end
+def starts_with_a_vowel?(word)
+ if word.match(/\b[aeiouAEIOU][a-z]*/)
+   true 
+ else 
+   false 
+ end 
+end 
 
 def words_starting_with_un_and_ending_with_ing(text)
 
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
